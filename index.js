c622dd5bc8f8693fd7d94349acfe10d3c38bf197
pi-3-1-4 <73631717+pi-3-1-4@users.noreply.github.com>
2023-03-17 10:49:19 +0000

    backend code uploaded for network speed test

---
+++ index.js
@@ -0,0 +1,28 @@
+const networkSpeed = require('network-speed');
+const testNetworkSpeed = new networkSpeed();
+
+async function getDownloadSpeed(){
+    const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
+    const fileSizeInBytes = 500000;
+    const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+    console.log("Download Speed=>",speed);
+}
+
+getDownloadSpeed();
+
+async function getUploadSpeed(){
+    const options = {
+        hostname: 'www.google.com',
+        port: 80,
+        path: '/catchers/544b09b4599c1d0200000289',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const fileSizeInBytes = 2000000
+      const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+      console.log("Upload Speed=>",speed);
+}
+
+getUploadSpeed()
